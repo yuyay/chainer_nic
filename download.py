9d15493ccb3a4e5354a0b483bad63648627b49e1
Yuya Yoshikawa <yosshi71jp@gmail.com>
2018-12-13 20:46:52 +0900

    Added --only-anno option to download annotation data only

--- download.py
+++ download.py
@@ -9,19 +9,26 @@ import subprocess
 """Download the MSCOCO dataset (images and captions)."""
 
 
-urls = [
-    'http://images.cocodataset.org/zips/train2014.zip',
-    'http://images.cocodataset.org/zips/val2014.zip',
-    'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
-    'https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/master/stair_captions_v1.2.tar.gz'
-]
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--out', type=str, default='data',
                         help='Target MSCOCO dataset root directory')
+    parser.add_argument('--only-anno', action="store_true",
+                        help='Whether to download annotation data only')
     args = parser.parse_args()
+
+    if args.only_anno:
+        urls = [
+            'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+            'https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/master/stair_captions_v1.2.tar.gz'
+        ]
+    else:
+        urls = [
+            'http://images.cocodataset.org/zips/train2014.zip',
+            'http://images.cocodataset.org/zips/val2014.zip',
+            'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+            'https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/master/stair_captions_v1.2.tar.gz'
+        ]
 
     try:
         os.makedirs(args.out)
